670c8f2dcd5eca9735ad1d90178372d9a0def9dc
Tomasz Szyborski <tomasz.szyborski@gmail.com>
2017-07-12 12:22:50 +0200

    Guided project: Analyzing wikipedia pages

---
+++ PySpark/PySpark_test.py
@@ -0,0 +1,14 @@
+# Find path to PySpark.
+import findspark
+findspark.init()
+
+# Import PySpark and initialize SparkContext object.
+import pyspark
+sc = pyspark.SparkContext()
+
+# Read `recent-grads.csv` in to an RDD.
+f = sc.textFile('recent-grads.csv')
+data = f.map(lambda line: line.split('\n'))
+t = data.take(10)
+
+print(t)
